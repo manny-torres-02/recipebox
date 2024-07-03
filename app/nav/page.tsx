56ed81c4f3567e3b7bacd44e72856a0887d060a3
Manuel Torres <manueljosetorres02@gmail.com>
2024-07-02 23:44:36 -0500

    test

---
+++ app/nav/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h1>Navigation Page</h1>
+      <p>This is the navigation page content.</p>
+    </nav>
+  );
+}
