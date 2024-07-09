18cf6b64e7cb17087ce52f55e555889e9db2713e
Manuel Torres <manueljosetorres02@gmail.com>
2024-07-09 00:03:55 -0500

    forms...

--- app/add_recipe/page.tsx
+++ app/add_recipe/page.tsx
@@ -1,94 +1,98 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function AddRecipe() {
-    return (
-      <div>
-        <p className="text-xl font-bold"> Add a Recipe </p>
-        <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-md mt-10">
-          <h1 className="text-2xl font-bold mb-6 text-center">
-            Submit a Recipe
-          </h1>
-          <form>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="recipeName"
-              >
-                Recipe Name
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="recipeName"
-                type="text"
-                placeholder="Recipe Name"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="ingredients"
-              >
-                Ingredients
-              </label>
-              <textarea
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="ingredients"
-                placeholder="List your ingredients here..."
-                rows="4"
-              ></textarea>
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="instructions"
-              >
-                Instructions
-              </label>
-              <textarea
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="instructions"
-                placeholder="List your instructions here..."
-                rows="6"
-              ></textarea>
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="notes"
-              >
-                Notes
-              </label>
-              <textarea
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="notes"
-                placeholder="Additional notes..."
-                rows="2"
-              ></textarea>
-            </div>
-            <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="photo"
-              >
-                Upload Photo
-              </label>
-              <input
-                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-                id="photo"
-                type="file"
-                accept="image/*"
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-              >
-                Submit Recipe
-              </button>
-            </div>
-          </form>
-        </div>
+const submitForm = (e) => {
+  e.preventDefault();
+
+  console.log("submitting form");
+};
+export default function AddRecipe(this: any) {
+  return (
+    <div>
+      <p className="text-xl font-bold"> Add a Recipe </p>
+      <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-md mt-10">
+        <h1 className="text-2xl font-bold mb-6 text-center">Submit a Recipe</h1>
+        <form onSubmit={submitForm}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="recipeName"
+            >
+              Recipe Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="recipeName"
+              type="text"
+              placeholder="Recipe Name"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="ingredients"
+            >
+              Ingredients
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="ingredients"
+              placeholder="List your ingredients here..."
+              rows={"4"}
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="instructions"
+            >
+              Instructions
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="instructions"
+              placeholder="List your instructions here..."
+              rows="6"
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="notes"
+            >
+              Notes
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="notes"
+              placeholder="Additional notes..."
+              rows="2"
+            ></textarea>
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="photo"
+            >
+              Upload Photo
+            </label>
+            <input
+              className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+              id="photo"
+              type="file"
+              accept="image/*"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick
+            >
+              Submit Recipe
+            </button>
+          </div>
+        </form>
       </div>
-    );
+    </div>
+  );
 }
