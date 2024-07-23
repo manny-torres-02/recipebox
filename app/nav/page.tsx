b7ef6c6bbf6b85f1a9498af1505625054349ca18
Manuel Torres <manueljosetorres02@gmail.com>
2024-07-22 23:22:52 -0500

    test

--- app/nav/page.tsx
+++ app/nav/page.tsx
@@ -39,7 +39,7 @@ export default function Nav() {
                 className="text-2xl font-bold px-4 py-2"
                 href="/view_recipe"
               >
-                ViewRecipe
+                RecipeList
               </Link>
             </div>
             {/* </div> */}
--- app/view_recipe/page.tsx
+++ app/view_recipe/page.tsx
@@ -1,6 +1,37 @@
 "use client";
 import React from "react";
+import { db } from "../../app/db/firebase.js";
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import useRecipeStore from "../store";
 
-export default function ViewRecipe() {
-  return <h1>hello</h1>;
+export default function RecipeList() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const querySnapshot = await getDocs(collection(db, "recipes"));
+      const recipeList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setRecipes(recipeList);
+    };
+
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.recipeName}</h3>
+          <p>Ingredients: {recipe.ingredients.join(", ")}</p>
+          <p>Instructions: {recipe.instructions}</p>
+          <p>Notes: {recipe.notes}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
