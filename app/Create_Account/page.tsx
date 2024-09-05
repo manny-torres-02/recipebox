7b3682ff268efe79458af08a3407d1c021348533
Manuel Torres <manueljosetorres02@gmail.com>
2024-09-04 23:34:11 -0500

    work on authentication

--- app/Create_Account/page.tsx
+++ app/Create_Account/page.tsx
@@ -2,13 +2,22 @@
 import React, { useState } from "react";
 
 export default function create_account() {
-  const [username, setNewUserName] = useState();
+  const [email, setEmail] = useState();
   const [password, setNewPassword] = useState();
-  const;
+  // const;
+
+  const handleSignup = async () => {
+    try {
+      const user
+    }
 
   return (
     <div>
-      <form></form>
+      <form>
+        <input type="text" value={email} placeholder="email" />
+        <input type="text" value={password} placeholder="password" />
+        <button type="submit"> Signup </button>
+      </form>
     </div>
   );
 }
